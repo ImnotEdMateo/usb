b296c5c4ef1cb9ee9b15f253c78cc3efecc7331a
ImnotEdMateo <mateolopez899@outlook.com>
2025-01-12 22:57:29 -0500

    shit

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+  "fmt"
 	"net/http"
   "github.com/imnotedmateo/ubs/handlers"
 )
 
 func main() {
+  fmt.Println("Iniciando la aplicación...")
+
 	// Sirve archivos estáticos
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", handlers.FileOrPageHandler)
 	http.HandleFunc("/upload", handlers.UploadHandler)
+
+  fmt.Println("Aplicación ejecutada correctamente")
 
 	http.ListenAndServe(":1488", nil)
 }
