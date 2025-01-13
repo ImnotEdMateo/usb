86d4d13b09949540e2c8c3f168d7b46ed8f7d286
ImnotEdMateo <mateolopez899@outlook.com>
2025-01-13 12:25:38 -0500

    chore: shit

--- main.go
+++ main.go
@@ -10,16 +10,13 @@ import (
 func main() {
 	fmt.Println("Iniciando la aplicación...")
 
-	// Sirve archivos estáticos
+	// Serve Static Files
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", handlers.FileOrPageHandler)
 	http.HandleFunc("/upload", handlers.UploadHandler)
 
-	fmt.Println("Aplicación ejecutada correctamente")
-
-	// Escuchar y servir el servidor con manejo de errores
 	if err := http.ListenAndServe(":1488", nil); err != nil {
-		log.Fatalf("Error al iniciar el servidor: %v", err)
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
